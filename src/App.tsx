25f8352415130ceada540cd9b2051e825073faec
Fefoler01 <72406859+Fefoler01@users.noreply.github.com>
2022-10-02 20:08:18 +0200

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import Nav from './components/Navbar';
+
+/* components */
+import Nav from './components/Navbar/navbar.components';
+import Footer from './components/Footer/footer.components';
 
 
 const App =  () => {
 	return (
 		<div className="App">
 			<Nav/>
+			<Footer/>
 		</div>
 	);
 };
