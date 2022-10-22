ba78d7986478be5b9353108c638480f41ddb36a7
Logan Le Lay <logan.lelay@hotmail.fr>
2022-10-22 17:47:38 +0200

    login+create_account

--- src/pages/create_account/create_account.component.tsx
+++ src/pages/create_account/create_account.component.tsx
@@ -1,20 +1,42 @@
 import {Link} from 'react-router-dom';
 
+import './create_account.styles.css';
+
 const CreateAccount = ():JSX.Element =>{
 	return (
 		<>
-			<p>Inscrivez vous</p>
+			<p className='register'><b>Register !</b></p>
 
-            <form>	
-				<input className="form-user" type="text"/>
-                <input className="form-password" type="password"/>
-                <input className="form-name" type="text"/>
-                <input className="form-surname" type="text"/>
-                <input className="form-age" type="text"/>
-				<button className="create_account">SignIn</button>
+			<form className="form-signin" method="post">
+				<div>
+					<label htmlFor ="form-user">User :</label>
+					<input className="form-user" type="text" name="form_user"/>
+				</div>
+				<div>
+					<label htmlFor ="form-password">Password : </label>
+					<input className="form-password" type="password" name="form_password"/>
+				</div>
+				<div>
+					<label htmlFor ="form-name">Name :</label>
+					<input className="form-name" type="text" name="form_name"/>
+				</div>
+				<div>
+					<label htmlFor ="form-surname">Surname :</label>
+					<input className="form-surname" type="text" name="form_surname"/>
+				</div>
+				<div>
+					<label htmlFor ="form-email">Email :</label>
+					<input className="form-email" type="email" name="form_email"/>
+				</div>
+				<div>
+					<label htmlFor ="form-age">Age :</label>
+					<input className="form-age" type="text" name="form_age"/>
+				</div>
+				<button className="create_account" type="submit">Sign in</button>
 			</form>
 
-			<Link to="../login">Deja inscrit ? Connectez vous !</Link>
+
+			<p className='login'>Already have an account ? <Link to="../login"><b>Login !</b></Link></p>
 
 		</>
 	);
--- src/pages/login/login.component.tsx
+++ src/pages/login/login.component.tsx
@@ -1,17 +1,25 @@
 import {Link} from 'react-router-dom';
 
+import './login.styles.css';
+
 const Login = ():JSX.Element =>{
 	return (
 		<>
-			<p>Connectez vous</p>
+			<p className='connect'><b>Connect yourself !</b></p>
 
-            <form>	
-				<input className="form-user" type="text"/>
-                <input className="form-password" type="password"/>
-				<button className="Login">Login</button>
+            <form className="form-login" method="get">
+				<div>
+					<label htmlFor ="form-user">User :</label>
+					<input className="form-user" type="text" name="form_user"/>
+				</div>
+				<div>
+					<label htmlFor ="form-password">Password : </label>
+					<input className="form-password" type="password" name="form_password"/>
+				</div>
+				<button className="Login" type="submit">Login</button>
 			</form>
 
-			<Link to="../create_account">Non inscrit ? Inscrivez vous !</Link>
+			<p className='signin'>Not register ? <Link to="../create_account"><b>Sign in !</b></Link></p>
 
 		</>
 	);
